0984b352774147d2d3d06fd23eef1c2bc8272dba
francis007-del <78894847+francis007-del@users.noreply.github.com>
2021-07-11 17:02:00 +0530

    Initial commit

---
+++ CMakeFiles/Hotel_Management_System_in_C_.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/francis/Downloads/Hotel-Management-System-in-C--main/app.cpp" "/home/francis/Downloads/Hotel-Management-System-in-C--main/CMakeFiles/Hotel_Management_System_in_C_.dir/app.cpp.o"
+  "/home/francis/Downloads/Hotel-Management-System-in-C--main/customer.cpp" "/home/francis/Downloads/Hotel-Management-System-in-C--main/CMakeFiles/Hotel_Management_System_in_C_.dir/customer.cpp.o"
+  "/home/francis/Downloads/Hotel-Management-System-in-C--main/hotel.cpp" "/home/francis/Downloads/Hotel-Management-System-in-C--main/CMakeFiles/Hotel_Management_System_in_C_.dir/hotel.cpp.o"
+  "/home/francis/Downloads/Hotel-Management-System-in-C--main/room.cpp" "/home/francis/Downloads/Hotel-Management-System-in-C--main/CMakeFiles/Hotel_Management_System_in_C_.dir/room.cpp.o"
+  "/home/francis/Downloads/Hotel-Management-System-in-C--main/user.cpp" "/home/francis/Downloads/Hotel-Management-System-in-C--main/CMakeFiles/Hotel_Management_System_in_C_.dir/user.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Hotel_Management_System_in_C_.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hotel_Management_System_in_C_.dir/app.cpp.o"
+  "CMakeFiles/Hotel_Management_System_in_C_.dir/customer.cpp.o"
+  "CMakeFiles/Hotel_Management_System_in_C_.dir/hotel.cpp.o"
+  "CMakeFiles/Hotel_Management_System_in_C_.dir/room.cpp.o"
+  "CMakeFiles/Hotel_Management_System_in_C_.dir/user.cpp.o"
+  "Hotel_Management_System_in_C_"
+  "Hotel_Management_System_in_C_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hotel_Management_System_in_C_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
